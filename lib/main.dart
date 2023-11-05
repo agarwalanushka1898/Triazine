f7ce1d5948275696c912988537bb63b896745d65
Jatin Saini <sainijatin5940@gmail.com>
2023-11-05 16:27:26 +0530

    navigation fixed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:triazine/presentation/screens/login_screen.dart';
 import 'package:triazine/presentation/screens/otp_screen.dart';
+import 'package:triazine/presentation/screens/splash_screen.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: OtpScreen(),
+      home: SplashScreen(),
     );
   }
 }
